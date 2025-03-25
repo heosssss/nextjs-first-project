7794ae9eec139ed49fd20ee233216018153f21d8
heosssss <appear1991@gmail.com>
2025-03-25 12:59:26 +0900

    동적 matadata 추가

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,11 +1,20 @@
 import { Suspense } from "react";
 import MoviesInfo from "../../../../component/movie-info";
 import MovieVideos from "../../../../component/movie-videos";
+import { getMovie } from "../../../../component/movie-info";
 
+interface IParams{
+    params:{id : string}
+}
 
-export default async function MoviesDetail({params}) {
-    const { id } = await params;
+export async function generateMetadata({params:{id}} : IParams){
+    const movie = await getMovie(id);
+    return {
+        title : movie.title
+    }
+}
 
+export default async function MoviesDetail({params:{id}} : IParams) {
     return <div> 
         <Suspense fallback={<h1>Loading movie info</h1>}>
             <MoviesInfo id={id} />
